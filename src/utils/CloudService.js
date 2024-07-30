739387d036ef2bd4f8cb9c58dd84b795240530ee
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-07-30 10:21:39 +0300

    fetch

--- src/utils/CloudService.js
+++ src/utils/CloudService.js
@@ -2,7 +2,7 @@ import Constants from "./Constants";
 
 const getResult = async (url) => {
     try {
-        const data = await fetch(url, { signal: AbortSignal.timeout(60000) })
+        const data = await fetch(url)
         return await data.json()
     } catch (error) {
         return {
