f17c1567729248535b9a67c6413cd7420e942029
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-08-06 11:42:00 +0300

    async result

--- src/utils/CloudService.js
+++ src/utils/CloudService.js
@@ -3,7 +3,44 @@ import Constants from "./Constants";
 const getResult = async (url) => {
     try {
         const data = await fetch(url)
-        return await data.json()
+        const json = await data.json()
+        if (json.type === 'sync') {
+            return json.data;
+        }
+
+        if (json.type === 'async') {
+            return await getAsyncResult(`${Constants.RESULT_URL}/${json.data}`)
+        }
+
+        return {
+            success: false,
+            message: 'unknown issue',
+        }
+    } catch (error) {
+        return {
+            success: false,
+            message: error.message,
+        }
+    }
+}
+
+const getAsyncResult = async (url) => {
+    try {
+        const data = await fetch(url)
+        const json = await data.json()
+        
+        if (json.success) {
+            return json
+        }
+
+        if (!json.success && json.status === 2) {
+            return await getAsyncResult(url);
+        }
+
+        return {
+            success: false,
+            message: 'unknown issue',
+        }
     } catch (error) {
         return {
             success: false,
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,9 @@
+const API_URL = 'https://api.vdovareize.me'
+// const API_URL = 'http://localhost:3000'
+
 export default {
-    FIBONACCI_URL: 'https://api.vdovareize.me/clouds/fibonacci',
-    PRIME_URL: 'https://api.vdovareize.me/clouds/prime',
-    ARMSTRONG_URL: 'https://api.vdovareize.me/clouds/armstrong',
+    FIBONACCI_URL: `${API_URL}/clouds/fibonacci`,
+    PRIME_URL: `${API_URL}/clouds/prime`,
+    ARMSTRONG_URL: `${API_URL}/clouds/armstrong`,
+    RESULT_URL: `${API_URL}/clouds/result`,
 }
