8536197a9e3c807a4702655f74302427fe0a77a0
Sergii Vdovareize <sergii.vdovareize@inspera.no>
2024-09-29 21:54:26 +0300

    updated api url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -5,5 +5,5 @@ export default {
     FIBONACCI_URL: `${API_URL}/clouds/fibonacci`,
     PRIME_URL: `${API_URL}/clouds/prime`,
     ARMSTRONG_URL: `${API_URL}/clouds/armstrong`,
-    RESULT_URL: `${API_URL}/clouds/result`,
+    RESULT_URL: `${API_URL}/async/result`,
 }
