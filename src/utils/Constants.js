576ba0caa8a7d5abbd85f7256566087f1321aa4a
kastet <kastet@weareclario.com>
2026-02-23 08:56:18 +0200

    refactor: explicitly export constants object and bump package version

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,9 +1,11 @@
 const API_URL = 'https://api.vdovareize.me'
 // const API_URL = 'http://localhost:3000'
 
-export default {
+const constants = {
     FIBONACCI_URL: `${API_URL}/clouds/fibonacci`,
     PRIME_URL: `${API_URL}/clouds/prime`,
     ARMSTRONG_URL: `${API_URL}/clouds/armstrong`,
     RESULT_URL: `${API_URL}/async/result`,
 }
+
+export default constants
