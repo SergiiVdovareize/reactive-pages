2d162ee1349e79064a4c14fb6c8cca69f7de3c89
Sergii Vdovareize <servdo@gmail.com>
2026-03-25 08:07:12 +0200

    chore: update dependencies to latest and fix test placeholder

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator inputs', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Get Armstrong number/i);
   expect(linkElement).toBeInTheDocument();
 });
